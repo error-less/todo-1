0e101cc35fed95948f8fa4927a84b1a63c86ddeb
Atul Singh <atul.ks1998@gmail.com>
2021-07-01 13:39:17 +0530

    day is set and sebd to ejs file

--- server.js
+++ server.js
@@ -5,7 +5,33 @@ app.set('view engine', 'ejs')
 app.use(express.static('public'))
 
 app.get('/',(req,res)=>{
-    res.render("list");
+    var d=new Date();
+    var current_day=d.getDay()
+    var day=""
+    switch(current_day){
+        case 0:
+            day="sunday";
+            break;
+        case 1:
+            day="Monday";
+            break;
+        case 2:
+            day="Tuesday";
+            break;
+        case 3:
+            day="Wednesday";
+            break;
+        case 4:
+            day="Thursday";
+            break;
+        case 5:
+            day="Friday";
+            break;
+        case 6:
+            day="Saturday";
+            break;
+    }
+    res.render("list",{day:day});
 })
 
 app.listen(5000,()=>{
