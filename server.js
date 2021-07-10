62eb3213f373c8a99b80ef1529163d159d1f2f96
Atul Singh <atul.ks1998@gmail.com>
2021-07-10 12:13:40 +0530

    added database

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser=require('body-parser');
+const mongoose = require('mongoose');
 const app = express();
 
 app.set('view engine', 'ejs')
@@ -7,24 +8,57 @@ app.use(express.static('public'))
 app.use(bodyParser.urlencoded({
     extended:true
 }))
-var items = ["take breakfast","take bath"];
+mongoose.connect('mongodb://localhost:27017/todolistDB', {useNewUrlParser: true, useUnifiedTopology: true});
+
+const itemsSchema = new mongoose.Schema({
+    name: String
+  });
+
+const Item = mongoose.model('Item', itemsSchema);
+
+const item1= new Item({
+    name:"welcome to todo"
+})
+const item2= new Item({
+    name:"hello there!"
+})
+const item3= new Item({
+    name:"Add your todo"
+})
+
+const defaultitems=[item1,item2,item3]
+Item.insertMany(defaultitems,function(err){
+    if(err){
+        console.log(err)
+    }
+    else{
+        console.log("successfully create db")
+    }
+})
 app.get('/', (req, res) => {
-    var d = new Date();
-    var options = {
-        day: 'numeric',
-        month: 'long',
-        year: 'numeric'
-    };
-    const day=(d.toLocaleDateString('en-ZA', options));
+    Item.find({},function(err,founditems){
+        res.render("list", {
+            listtitle: "Today",
+            listitems:founditems
+        });
+    })
+   
+})
+app.get('/work',(req, res)=>{
     res.render("list", {
-        kindday: day,
-        listitems:items
-    });
+        listtitle: "work",
+        listitems:workitems
+    })
 })
 app.post('/', (req, res) => {
     var item =req.body.task
+    if(req.body.list==="work"){
+        workitems.push(item);
+        res.redirect('/work')
+    }
+    else{
     items.push(item);
-    res.redirect('/');
+    res.redirect('/');}
 })
 
 app.listen(3030, () => {
